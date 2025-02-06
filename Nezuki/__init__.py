1824000125270c22b8f44727b975388561ae8e96
Casetta Server <djstrix@me.com>
2025-02-06 21:12:34 +0000

    Fix init

--- Nezuki/__init__.py
+++ Nezuki/__init__.py
@@ -1,10 +1,9 @@
+__version__ = "1.0.1"
 from .Database.Database import Database
 from .Http.Http import Http
-from .APIModules.APIModules import APIModules
 from .Bot.Bot import Bot
-from .commonLoggerSingleTon.commonLoggerSingleTon import commonLoggerSingleTon
-from .commonLogId.commonLogId import commonLogId
-from .Common.Common import Common
+from .commonLoggerSingleTon import commonLoggerSingleTon
+from .commonLogId import commonLogId
 from .Database.Database import Database
 from .Documentor.Documentor import Documentor
 from .File.File import File
@@ -15,14 +14,14 @@ from .JsonManager.JsonManager import JsonManager
 from .LogContext.LogContext import LogContext
 from .Logger.Logger import Logger
 from .Mail.Mail import Mail
-from .PassKey.PassKey import PassKey
+from .PassKey import PassKey
 from .PyModules.PyModules import PyModules
 from .QrCode.QrCode import QrCode
 from .ServerUtils.ServerUtils import ServerUtils
 from .TelegramApi.TelegramApi import TelegramApi
 from .TelegramParser.TelegramParser import TelegramParser
-from .Telegram.Telegram import Telegram
-from .WebLogsViewer.WebLogsViewer import WebLogsViewer
+from .Telegram.Telegram import TelegramParser_OLD
+from .WebLogsViewer import WebLogsViewer
 from .YamlManager.YamlManager import YamlManager
 
 __all__ = ['APIModules', 'Bot', 'commonLoggerSingleTon', 'commonLogId', 'Common', 'Database', 'Documentor', 'File', 'GeneratoreCodici', 'HomeConnection', 'Http', 'JsonManager', 'LogContext', 'Logger', 'Mail', 'PassKey', 'PyModules', 'QrCode', 'ServerUtils', 'TelegramApi', 'TelegramParser', 'Telegram', 'WebLogsViewer', 'YamlManager']
