1ffee31fe946a6cbca5c7dd77718c6461d5ebb0c
Sergio Catacci <djstrix@me.com>
2025-02-14 23:14:53 +0100

    Added unit test (#25)

--- nezuki/Http/__init__.py
+++ nezuki/Http/__init__.py
@@ -1,5 +1,5 @@
 __version__ = "2.0.0"
 
-from .Http import Http
+from .Http import Http, InsufficientInfo, MethodNotSupported
 
-__all__ = ['Http']
+__all__ = ['Http', 'InsufficientInfo', 'MethodNotSupported']
