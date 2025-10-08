f6903f75ac76b0a7d68603ac589c8f492910fe86
Sergio Catacci <>
2025-10-08 13:07:17 +0200

    feat: aggiorna la gestione della versione e migliora il decoratore di deprecazione

--- nezuki/__init__.py
+++ nezuki/__init__.py
@@ -1,1 +1,3 @@
-__version__ = "1.2.1"
+import os
+with open(os.path.join(os.path.dirname(__file__), "..", "version.txt"), "r") as vf:
+     __version__ = vf.read().strip()
--- versioning.py
+++ versioning.py
@@ -1,4 +1,6 @@
 import functools, warnings
+from packaging import version
+from nezuki import __version__ as nezuki_module_version
 from nezuki.Logger import get_nezuki_logger
 
 logger = get_nezuki_logger()
@@ -15,9 +17,18 @@ def deprecated(deprecated_version: str, new_alternative: str):
     def decorator(func):
         @functools.wraps(func)
         def wrapper(*args, **kwargs):
-            msg = f"{func.__name__} è deprecata, supporto garantito fino alla versione {deprecated_version}, ora sei alla versione {func.__version__}. {new_alternative} al suo posto."
+            current = version.parse(func.__version__)
+            deprecated = version.parse(deprecated_version)
+            msg = (
+                f"{func.__name__} è deprecata dalla versione {deprecated_version}, attualmente sei alla {current}.\t"
+                f"Note: {new_alternative}"
+                f"\tNezuki version: {nezuki_module_version}"
+            )
             warnings.warn(msg, category=DeprecationWarning, stacklevel=2)
-            logger.warning(f"DEPRECATION WARNING: {msg}", extra={"internal": True})
+            if current > deprecated:
+                logger.error(f"DEPRECATION ERROR: {msg}", extra={"internal": True})
+            else:
+                logger.warning(f"DEPRECATION WARNING: {msg}", extra={"internal": True})
             return func(*args, **kwargs)
         return wrapper
     return decorator
