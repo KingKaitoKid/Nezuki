de5360db255bd5d8676407f3519b7223d4e224e2
Sergio <kaito@rpi5-nvme-apache.lan>
2025-11-25 10:52:52 +0100

    BUMP 2.2.5

--- nezuki/__init__.py
+++ nezuki/__init__.py
@@ -1,4 +1,4 @@
 import os
 from pathlib import Path
-with open(os.path.join(os.path.dirname(__file__), "version.txt"), "r") as vf:
+with open(os.path.join(os.path.dirname(__file__), "version.py"), "r") as vf:
      __version__ = vf.read().strip()
